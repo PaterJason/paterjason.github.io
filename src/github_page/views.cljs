ff2a3488c2c42a2e45c1c383d05ddc403c08fa64
Jason Paterson <jasonbipaterson@gmail.com>
2022-01-28 20:46:08 +0000

    Use github action to build js

--- src/github_page/views.cljs
+++ src/github_page/views.cljs
@@ -1,12 +1,10 @@
 (ns github-page.views
   (:require
    [re-frame.core :as re-frame]
-   [github-page.subs :as subs]
-   ))
+   [github-page.subs :as subs]))
 
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
     [:div
      [:h1
-      "Hello from " @name]
-     ]))
+      "Henlo " @name]]))
